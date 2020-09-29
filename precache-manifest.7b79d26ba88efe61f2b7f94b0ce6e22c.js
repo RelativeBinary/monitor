ed3c67042fb03dd8715cf43c6a2215646ae5f367
PatrickBornay <patrick.bornay@gmail.com>
2020-09-29 14:30:54 +1000

    Updates

---
+++ precache-manifest.7b79d26ba88efe61f2b7f94b0ce6e22c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cabaf7026decf15ea68678257b4606f",
+    "url": "/monitor/index.html"
+  },
+  {
+    "revision": "028ee016433c52660702",
+    "url": "/monitor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d6ad4fc29285e1d20ec",
+    "url": "/monitor/static/js/2.cb5ac6e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monitor/static/js/2.cb5ac6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028ee016433c52660702",
+    "url": "/monitor/static/js/main.f434b999.chunk.js"
+  },
+  {
+    "revision": "be0c8fe0edc1f519e9a1",
+    "url": "/monitor/static/js/runtime-main.ae3f48d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monitor/static/media/logo.5d5d9eef.svg"
+  }
+]);
